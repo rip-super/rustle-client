4c69245d1f4831f6a37f6c01ac8261c8b62dfc6a
Sahil Dash <sahildash7704@gmail.com>
2025-05-26 20:43:20 -0500

    added dynamic window resizing based on screen resolution

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,7 @@
 //     format!("Hello, {}! You've been greeted from Rust!", name)
 // }
 
-use tauri::Manager;
+use tauri::{LogicalSize, Manager};
 
 fn main() {
     tauri::Builder::default()
@@ -15,8 +15,24 @@ fn main() {
         //.invoke_handler(tauri::generate_handler![greet])
         .setup(|app| {
             let window = app.get_webview_window("main").unwrap();
+
+            let monitor = window.primary_monitor().unwrap().unwrap();
+            let size = monitor.size();
+            let mut new_width = size.width as f64 * (1280.0 / 3440.0);
+            let mut new_height = size.height as f64 * (720.0 / 1440.0);
+
+            if new_width < 800.0 || new_height < 450.0 {
+                new_width = 800.0;
+                new_height = 450.0;
+            }
+
+            window
+                .set_size(LogicalSize::new(new_width, new_height))
+                .unwrap();
+
             window.center().unwrap();
             window.show().unwrap();
+
             Ok(())
         })
         .run(tauri::generate_context!())
